9873416b0fa5a734e9ca6ff174a89d1e0fa86c06
patryk.piwowarczyk <patryk.piwowarczyk@dazn.com>
2022-07-12 14:31:20 +0200

    Initial coming of combined rxSwift part and UIkit part

--- DAZN/ViewController.swift
+++ DAZN/ViewController.swift
@@ -6,6 +6,11 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+var boolValEmail = false
+var boolValPassword = false
 
 class ViewController: UIViewController {
 
@@ -18,6 +23,9 @@ class ViewController: UIViewController {
     let eyeButton = UIButton()
     var showPassword = false
     
+    
+    private let loginViewModel = LoginViewModel()
+    let disposeBag = DisposeBag()
     
     // animationsValues
     
@@ -38,6 +46,35 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor(named: "Tarmac")
         
     
+        loginTextField.becomeFirstResponder()
+        
+        //Starting the Reactive publishing for both the Email and Password fields. Connecting them with "loginViewModel" so they can be published as events there.
+        loginTextField.rx.text.map { $0 ?? ""}.bind(to: loginViewModel.usernameTextPublishSubject).disposed(by: disposeBag)
+        passwordTextField.rx.text.map { $0 ?? ""}.bind(to: loginViewModel.passwordTextPublishSubject).disposed(by: disposeBag)
+        
+        
+        //Uncovering the login button when BOTH EMAIL and PASSWORD are valid.
+        loginViewModel.isValid().bind(to: loginButton.rx.isEnabled).disposed(by: disposeBag)
+        loginViewModel.isValid().map { $0 ? 1 : 0.2}.bind(to: loginButton.rx.alpha).disposed(by: disposeBag)
+        
+        
+        //Setting the one time bool value to true when user stops editing the Email or Password for the first time.
+        //Thanks to that, user isn't notified abour wrong password at the moment of beggining of filling the form.
+        loginTextField.rx.controlEvent([.editingDidEndOnExit]).subscribe(onNext:{ boolValEmail = true})
+        passwordTextField.rx.controlEvent([.editingDidEndOnExit]).subscribe(onNext:{ boolValPassword = true})
+        
+        loginTextField.rx.controlEvent([.editingDidBegin]).subscribe(onNext:{ boolValEmail = false})
+        passwordTextField.rx.controlEvent([.editingDidBegin]).subscribe(onNext:{ boolValPassword = false})
+        
+        /*
+         
+         TO ZROBIC JAK BEDA CZERWONE PRZYPOMINACZE O ZLYM HASLE I ZLYM LOGINIE!!
+         
+        //Setting the "INCORRECT EMAIL" and "INCORRECT PASSWORD" Red Labels Invisible and visible depending on the "isEmailValid" and "isPasswordValid"
+        loginViewModel.isEmailValid().map {$0 ? 1 : 0.01 }.bind(to: incorrectEmail.rx.alpha).disposed(by: disposeBag)
+        loginViewModel.isPasswordValid().map {$0 ? 1 : 0.01 }.bind(to: incorrectPassword.rx.alpha).disposed(by: disposeBag)
+    
+        */
     }
        
 }
@@ -164,6 +201,71 @@ extension ViewController{
         animator.startAnimation()
     }
 
-
+    @IBDesignable
+    class LoginViewModel: UIControl {
+        
+        
+        
+        let usernameTextPublishSubject = PublishSubject<String>()
+        let passwordTextPublishSubject = PublishSubject<String>()
+        
+        
+        // isValid checks if BOTH email and password are correct
+        
+        func isValid() -> Observable<Bool> {
+           return Observable.combineLatest(usernameTextPublishSubject.asObservable().startWith(""), passwordTextPublishSubject.asObservable().startWith("")).map {
+                username , password in
+               return username.isValidEmail && password.isValidPassword
+            }.startWith(false)
+        }
+         
+        //isEmailValid checks if email is valid
+        
+        func isEmailValid() -> Observable<Bool> {
+            
+            return Observable.combineLatest(usernameTextPublishSubject.asObservable().startWith(""), passwordTextPublishSubject.asObservable().startWith("")).map {
+                username , password in
+               return !username.isValidEmail && boolValEmail == true
+            }.startWith(false)
+            
+            
+        }
+        
+        
+        //checks if password is valid
+        
+        func isPasswordValid() -> Observable<Bool> {
+            
+           return Observable.combineLatest(usernameTextPublishSubject.asObservable().startWith(""), passwordTextPublishSubject.asObservable().startWith("")).map {
+                username , password in
+               return !password.isValidPassword && boolValPassword == true
+            }.startWith(false)
+        }
+        
+    // INITIALIZATION
+        var email = ""
+        var password = ""
+        
+        convenience init(email : String, password : String) {
+            self.init()
+            self.email = email
+            self.password = password
+        }
+        
+        
+    }
 }
 
+extension String {
+    var isValidEmail: Bool {
+        
+        
+        NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,}").evaluate(with: self)
+      
+        
+      
+    }
+    var isValidPassword: Bool {
+      NSPredicate(format: "SELF MATCHES %@ ", "^(?=.*[a-z])(?=.*[0-9])(.*[A-Z0-9a-z._%+-]).{5,}$").evaluate(with: self)
+    }
+}
