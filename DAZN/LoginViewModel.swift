912063c4a4b5b29a9f14a3f6474130d6f61d977b
patryk.piwowarczyk <patryk.piwowarczyk@dazn.com>
2022-07-13 09:37:52 +0200

    1 to 2

--- DAZN/LoginViewModel.swift
+++ DAZN/LoginViewModel.swift
@@ -67,7 +67,7 @@ extension String {
 var isValidEmail: Bool {
     
     
-    NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,}").evaluate(with: self)
+    NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
   
     
   
